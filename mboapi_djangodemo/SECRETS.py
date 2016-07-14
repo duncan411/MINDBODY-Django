7ecfb1fb6cbce0ad162c43bf87079064d3dd4944
dunks <duncan@egroup.com.au>
2016-07-14 17:12:18 +1000

    Update SECRETS.py

--- mboapi_djangodemo/SECRETS.py
+++ mboapi_djangodemo/SECRETS.py
@@ -1,13 +1,13 @@
-
-SOURCENAME = ''  #SourceName to be used in all API Calls in this project
-SOURCEPASS = ''  #SourceName Password to be used in all API calls
+SOURCENAME = 'WeeBroWeb'  #SourceName to be used in all API Calls in this project
+SOURCEPASS = 'xtGarFCNYZ9Q91jnH4Tpw50uqpg='  #SourceName Password to be used in all API calls
 
 #this needs to be set by the developer in whatever way they choose, based on user input
-USERNAME = ''  
-USERPASS = ''  
+USERNAME = 'caitlin@summerhealingyoga.com.au'  
+USERPASS = 'x5xekusWA'  
 
 # SiteIDs to be used in all API Calls - should ALWAYS match for SourceCredentials and UserCredentials nodes.
-SITEIDs = []
+#SITEIDs = [25809,204197,281886]
+SITEIDs = [204197]
 
 
 # optional stuff if you have a mail service.  handles contact form stuff, and whatever else the developer decides to hook up
